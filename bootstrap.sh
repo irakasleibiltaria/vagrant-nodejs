baeed51e07f843b18126c76c3810809df4d05344
irakasleibiltaria <irakasleibiltaria@gmail.com>
2015-06-17 09:17:35 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 #apt-get install -y python-software-properties
-#apt-get install -y software-properties-common
+apt-get install -y software-properties-common
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
 apt-get install -y nodejs
